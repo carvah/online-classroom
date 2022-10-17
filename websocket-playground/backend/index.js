da3d9ebf6d4cbe5c0829387c7dbee13115c92e61
Rafael Carvalho <rafael.carvalho@buildium.com>
2022-10-17 15:17:03 +0100

    initial commit

---
+++ websocket-playground/backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('frontend'));
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
+
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
+
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
