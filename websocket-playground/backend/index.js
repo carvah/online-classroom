14d8e3cd28db6ff0850ea5f4c3d325b11c2cbc67
Rafael Carvalho <rafael.carvalho@buildium.com>
2022-10-17 15:27:39 +0100

    set port dynamic;

--- websocket-playground/backend/index.js
+++ websocket-playground/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('frontend'));
 
@@ -15,6 +16,6 @@ io.on('connection', function(socket) {
    });
 });
 
-http.listen(3000, function() {
-   console.log('listening on *:3000');
+http.listen(PORT, function() {
+   console.log(`listening on *:${PORT}`);
 });
