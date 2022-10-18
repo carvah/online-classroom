ce5b8fe695f6d70f41bac9a665a7fc5e797ee013
Rafael Carvalho <rafael.carvalho@wikialphabet.com>
2022-10-18 01:35:38 +0100

    make simple test

--- websocket-playground/backend/index.js
+++ websocket-playground/backend/index.js
@@ -1,21 +1,96 @@
+const { Console } = require('console');
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const PORT = process.env.PORT || 3000;
+const TICK_RATE = 128;
+
+const getGravity = () => {
+    const meterInPixels = 3779.5275590551; // 9.2 meters per second
+    const gravity = TICK_RATE / meterInPixels;
+    return gravity;
+}
+
+const GRAVITY = 0;
+
+const PLAYER_SPEED = 5.0;
 
 app.use(express.static('frontend'));
 
+const playersSocketMap = {};
+const socketMap = {};
+const controlsMap = {};
+const players = [];
+const CONTROLS = {
+    UP: "up",
+    DOWN: "down",
+    LEFT: "left",
+    RIGHT: "right",
+    JUMP: "jump"
+};
+
 //Whenever someone connects this gets executed
 io.on('connection', function(socket) {
-   console.log('A user connected');
+    console.log('A user connected');
 
-   //Whenever someone disconnects this piece of code executed
-   socket.on('disconnect', function () {
-      console.log('A user disconnected');
-   });
+    //Whenever someone disconnects this piece of code executed
+    socket.on('disconnect', function() {
+        console.log('A user disconnected');
+    });
+
+    socket.on('join', (player) => {
+        console.log(`${player.name} connected`);
+
+        const newPlayer = {
+            x: 100,
+            y: 100,
+            vx: 0,
+            vy: 0,
+            name: player.name,
+            id: socket.id,
+            color: `#${Math.floor(Math.random() * (0xffffff + 1)).toString(16)}`,
+        }
+
+        playersSocketMap[socket.id] = newPlayer;
+        socketMap[socket.id] = socket;
+
+        players.push(newPlayer);
+    });
+
+    socket.on("controls", (controls) => {
+        controlsMap[socket.id] = controls;
+    });
 });
 
+
+function tick(delta) {
+    players.forEach(player => {
+        const playerControls = controlsMap[player.id] ?? {};        
+        player.vy += GRAVITY * delta;
+        player.y += player.vy;
+
+
+        if (playerControls[CONTROLS.RIGHT]) {
+            player.x += PLAYER_SPEED;
+        }
+
+        if (playerControls[CONTROLS.LEFT]) {
+            player.x -= PLAYER_SPEED;
+        }
+    });
+
+    io.emit('players', players);
+}
+
+let lastUpdated = Date.now();
+setInterval(() => {
+    const now = Date.now();
+    const delta = now - lastUpdated;
+    tick(delta);
+    lastUpdated = now;
+}, 1000 / TICK_RATE);
+
 http.listen(PORT, function() {
-   console.log(`listening on *:${PORT}`);
+    console.log(`listening on *:${PORT}`);
 });
--- websocket-playground/frontend/index.js
+++ websocket-playground/frontend/index.js
@@ -1,5 +1,99 @@
-const test = document.getElementById('test');
+const loginButton = document.getElementById('loginButton');
+const loginControls = document.getElementById('loginControls');
+const gameControl = document.getElementById('gameControl');
+const nameEl = document.getElementById('name');
 
-test.addEventListener('click', () => {
-    console.log('lenda');
-})
+
+const player = {
+    name: 'none'
+};
+
+const socket = io();
+const canvas = document.getElementById('canvas');
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+
+let lastRender = 0;
+let players = [];
+
+const controls = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+    jump: false,
+};
+
+const keyMap = {
+    w: "up",
+    s: "down",
+    a: "left",
+    d: "right",
+    " ": "jump",
+};
+
+
+loginButton.addEventListener('click', () => {
+    login();
+});
+
+document.addEventListener("keydown", (e) => {
+    controls[keyMap[e.key]] = true;
+});
+
+document.addEventListener("keyup", (e) => {
+    controls[keyMap[e.key]] = false;
+});
+
+
+function login() {
+    loginControls.style.display = 'none';
+    gameControl.style.display = 'block';
+    player.name = nameEl.value;
+
+    socket.emit('join', player);
+}
+
+socket.on('players', (serverPlayers) => {
+    players = serverPlayers;
+});
+
+function update(delta) {
+    socket.emit("controls", controls);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, width, height);
+
+    players.forEach(player => {
+        // is mine
+        if (player.id === socket.id) {
+            console.log(player);
+            ctx.fillStyle = player.color;
+            ctx.fillRect(player.x, player.y, 50, 50);
+        }
+
+        ctx.fillStyle = player.color;
+        ctx.fillRect(player.x, player.y, 50, 50);
+        ctx.fillStyle = "#000000";
+        ctx.fillText(player.name, player.x + (50 / 2), player.y - 10);
+    });
+}
+
+
+function loop(timestamp) {
+    const delta = timestamp - lastRender;
+
+    update(delta);
+    draw();
+
+    lastRender = timestamp;
+    window.requestAnimationFrame(loop);
+}
+window.requestAnimationFrame(loop);
