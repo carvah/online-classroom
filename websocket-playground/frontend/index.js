ba47986b3dccf5142cbc8c44bafade506b88b853
Rafael Carvalho <rafael.carvalho@wikialphabet.com>
2022-10-19 00:25:38 +0100

    camera control and FPS;

--- websocket-playground/frontend/index.js
+++ websocket-playground/frontend/index.js
@@ -2,20 +2,22 @@ const loginButton = document.getElementById('loginButton');
 const loginControls = document.getElementById('loginControls');
 const gameControl = document.getElementById('gameControl');
 const nameEl = document.getElementById('name');
-
+const canvas = document.getElementById('canvas');
 
 const player = {
     name: 'none'
 };
 
 const socket = io();
-const canvas = document.getElementById('canvas');
+const aspectRatio = 16 / 9;
 const width = window.innerWidth;
-const height = window.innerHeight;
+const height = window.innerHeight - 150;
+
+let fpsInMs = 0;
+let latency = 0;
 
 canvas.width = width;
 canvas.height = height;
-
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = 'red';
 
@@ -68,41 +70,54 @@ socket.on('players', (serverPlayers) => {
     players = serverPlayers;
 });
 
-socket.on('sendMap', serverMap => {
+socket.on('sendMap', (serverMap) => {
     map = serverMap;
 });
 
 setInterval(() => {
-    socket.emit('ping', Date.now());    
+    socket.emit('ping', Date.now());
 }, 1000);
 
-let latency = 0;
-socket.on('pong', (pong) => {    
+
+socket.on('pong', (pong) => {
     latency = Date.now() - pong;
-    console.log(latency);
+    fpsInMs = latency;
+    const fpsStatEl = document.getElementById('fps_stat');
+    if (fpsStatEl) {
+        fpsStatEl.value = `${fpsInMs} ms`;
+    }
 });
 
 function update(delta) {
     socket.emit("controls", controls);
 }
 
-function draw() {
-    ctx.clearRect(0, 0, width, height);
-
-    drawMap();
-
+let cameraX = 0;
+let cameraY = 0;
+const drawPlayers = () => {
     players.forEach(player => {
-        // is mine
+        // is mine        
         if (player.id === socket.id) {
-            ctx.fillStyle = player.color;
-            ctx.fillRect(player.x, player.y, PLAYER_SIZE, PLAYER_SIZE);
+            cameraX = player.x - canvas.width / 2;
+            cameraY = player.y - canvas.height / 2;
+            ctx.fillStyle = '#FFF301';
+            ctx.fillRect(player.x - cameraX, player.y - cameraY, PLAYER_SIZE, PLAYER_SIZE);
         }
 
         ctx.fillStyle = player.color;
-        ctx.fillRect(player.x, player.y, PLAYER_SIZE, PLAYER_SIZE);
+        ctx.fillRect(player.x - cameraX, player.y - cameraY, PLAYER_SIZE, PLAYER_SIZE);
         ctx.fillStyle = "#000000";
-        ctx.fillText(player.name, player.x + (PLAYER_SIZE / 2), player.y - 10);
+
+        ctx.textBaseline = 'middle';
+        ctx.textAlign = 'center';
+        ctx.fillText(player.name, player.x - cameraX, (player.y - 10) - cameraY);
     });
+};
+
+function draw() {
+    ctx.clearRect(0, 0, width, height);
+    drawPlayers();
+    drawMap();
 }
 
 const drawMap = () => {
@@ -112,8 +127,8 @@ const drawMap = () => {
             const tileType = map[row][col];
             if (tileType === 1) {
                 ctx.fillRect(
-                    col * TILE_SIZE,
-                    row * TILE_SIZE,
+                    col * TILE_SIZE - cameraX,
+                    row * TILE_SIZE - cameraY,
                     TILE_SIZE,
                     TILE_SIZE
                 );
